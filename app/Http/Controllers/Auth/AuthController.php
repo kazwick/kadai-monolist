eb36fb7f560ba803306fc4d92d5448c0a7aafd0f
kazwick <kaz0622@gmail.com>
2017-09-26 05:01:17 +0000

    user login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -20,7 +20,6 @@ class AuthController extends Controller
     | a simple trait to add these behaviors. Why don't you explore it?
     |
     */
-
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
     /**
@@ -28,6 +27,13 @@ class AuthController extends Controller
      *
      * @return void
      */
+
+    //追加
+    protected $redirectTo = '/';
+    protected $loginPath = '/login';
+    
+
+
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'getLogout']);
@@ -62,5 +68,5 @@ class AuthController extends Controller
             'password' => bcrypt($data['password']),
         ]);
     }
-    protected $redirectTo = '/';
+
 }
